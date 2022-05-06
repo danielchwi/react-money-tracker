26dff91f780d6b8292d5fed166950c3ffaba8f44
danielchwi <daniel.widodo@bitcoin.co.id>
2022-05-06 11:08:50 +0700

    Render after isAuthReady and guarding routes & redirect

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
 //import './App.css'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { isAuthReady, user } = useAuthContext()
   return (
     <div className="App">
+
+      {isAuthReady && (
       <BrowserRouter>
 
         <Navbar/>
 
         <Switch>
           <Route exact path='/'>
-            <Home />
+            {user && <Home />}
+            {!user && <Redirect to={"/login"} />}
           </Route>
         </Switch>
         
         <Switch>
           <Route exact path='/login'>
-            <Login />
+            {!user && <Login />}
+            {user && <Redirect to={"/"} />}
           </Route>
         </Switch>
 
         <Switch>
           <Route exact path='/signup'>
-            <Signup />
+            {!user && <Signup />}
+            {user && <Redirect to={"/"} />}
           </Route>
         </Switch>
       
-      
       </BrowserRouter>
+      )}
+
     </div>
   );
 }
